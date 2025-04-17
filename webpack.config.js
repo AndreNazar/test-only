57e96d746201ee894551b18e50696e7a5ea0ab07
AndreNazar <callandrnaz@gmail.com>
2025-04-17 21:10:48 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'assets/imgs/[name][ext]'
+        }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
